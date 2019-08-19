783e4cec4112911f68729658fb3fa8857f2fe34d
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-08-19 10:48:35 +0100

    Update the expected response message

--- PostFeedbackHttpTrigger/tests/test_main_azure_function.py
+++ PostFeedbackHttpTrigger/tests/test_main_azure_function.py
@@ -1,4 +1,3 @@
-
 import json
 import unittest
 from unittest import mock
@@ -12,17 +11,14 @@ from PostFeedbackHttpTrigger import main
 class TestMainAzureFunction(unittest.TestCase):
     @mock.patch("feedback_creator.utils.get_collection_link")
     @mock.patch("feedback_creator.utils.get_cosmos_client")
-    def test_with_missing_mandatory_param(self, mock_get_collection_link, mock_get_cosmos_client):
+    def test_with_missing_mandatory_param(
+        self, mock_get_collection_link, mock_get_cosmos_client
+    ):
 
-
-        invalid_feedback_body = get_string('fixtures/missing_is_useful.json')
-        invalid_feedback_body = bytearray(invalid_feedback_body, 'utf8')
+        invalid_feedback_body = get_string("fixtures/missing_is_useful.json")
+        invalid_feedback_body = bytearray(invalid_feedback_body, "utf8")
         # Construct a mock HTTP request.
-        req = func.HttpRequest(
-            method="POST",
-            body=invalid_feedback_body,
-            url=f"/api/",
-        )
+        req = func.HttpRequest(method="POST", body=invalid_feedback_body, url=f"/api/")
 
         # Call the main Azure Function entry point with the request.
         resp = main(req)
@@ -30,7 +26,6 @@ class TestMainAzureFunction(unittest.TestCase):
         # Check status code
         self.assertEqual(resp.status_code, 400)
 
-"""
         # Check content type
         headers = dict(resp.headers)
         self.assertEqual(headers["content-type"], "application/json")
@@ -39,10 +34,9 @@ class TestMainAzureFunction(unittest.TestCase):
         error_msg = json.loads(resp.get_body().decode("utf-8"))
         self.assertEqual(error_msg["errors"][0]["error"], "Bad Request")
         self.assertEqual(
-            error_msg["errors"][0]["error_values"][0]["Parameter Error"],
-            "Invalid parameter passed",
+            error_msg["errors"][0]["error_values"][0]["JSON Validation Error"],
+            "'is_useful' is a required property",
         )
-"""
 
 
 # TODO add more tests
